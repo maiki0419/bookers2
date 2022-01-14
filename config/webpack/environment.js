7f34e45fec5d1de960c57e8b4afc3b3f69f44591
maiki0419 <95464965+maiki0419@users.noreply.github.com>
2022-01-14 11:45:57 +0000

    [Finish]application-2

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,8 @@
 const { environment } = require('@rails/webpacker')
 
+module.exports = environment
+
+
 const webpack = require('webpack')
 environment.plugins.prepend(
   'Provide',
@@ -9,5 +12,3 @@ environment.plugins.prepend(
     Popper: 'popper.js'
   })
 )
-
-module.exports = environment
