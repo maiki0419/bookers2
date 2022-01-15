f297a0514211b8bc1ca2d28d5a51e888b8a8549c
maiki0419 <95464965+maiki0419@users.noreply.github.com>
2022-01-15 11:17:24 +0000

    [start]application-3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,8 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @book_new = Book.new
+    @book_comment = BookComment.new
+    @book_comments = @book.book_comments
     @user = @book.user
   end
 
