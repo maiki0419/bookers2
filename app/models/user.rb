b01970cb072b7bb6b0be0d50c4044bea3920c46a
maiki0419 <95464965+maiki0419@users.noreply.github.com>
2022-01-16 11:20:31 +0000

    [Finish]application-3

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,13 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites, dependent: :destroy
   has_many :book_comment
+
+  has_many :relationships, foreign_key: :following_id
+  has_many :followings, through: :relationships, source: :follower
+
+  has_many :reverse_of_relationships, class_name:"Relationship",foreign_key: :follower_id
+  has_many :followers, through: :reverse_of_relationships, source: :following
+
   has_one_attached :profile_image
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction,length: {maximum: 50}
@@ -16,4 +23,9 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
+  def followed_by?(user)
+    reverse_of_relationships.find_by(following_id: user.id).present?
+  end
+
 end
