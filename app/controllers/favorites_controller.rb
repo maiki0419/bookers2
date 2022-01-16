92b57afa2c7fc45a8a9c2fa0bd6350e4f5ff13e5
maiki0419 <95464965+maiki0419@users.noreply.github.com>
2022-01-16 00:06:04 +0000

    [Finish]application-3

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,18 @@
 class FavoritesController < ApplicationController
+
   def create
-    @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: @book.id)
+    favorite = current_user.favorites.new(book_id: params[:book_id])
     favorite.save
+    redirect_to request.referer
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: @book.id)
+    favorite = current_user.favorites.find_by(book_id: params[:book_id])
     favorite.destroy
+    redirect_to request.referer
   end
 
   private
+
 
 end
