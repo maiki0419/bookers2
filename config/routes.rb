8bb4446ace0b60973225b4da84004ef9c94cc2f4
maiki0419 <95464965+maiki0419@users.noreply.github.com>
2022-01-17 07:03:45 +0000

    [Finish]application-5

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
   get 'home/about'=>'homes#about'
+  get '/searches/search' => 'searches#search',as: 'searches'
   devise_for :users
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create,:destroy]
